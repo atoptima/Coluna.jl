99d7e88e0f0304ef38dd5c2c4eb9145806476a2e
Vitor Nesello <vitornesello@gmail.com>
2018-09-21 14:49:42 +0200

    updating runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,10 +14,12 @@ include("utils.jl")
 include("colunabasictests.jl")
 include("colgenroot.jl")
 include("moi_wrapper.jl")
+include("unit_tests/unit_tests.jl")
 
 # using Base.CoreLogging, Logging
 # global_logger(ConsoleLogger(stderr, LogLevel(-5)))
 
+unit_tests()
 testdefaultbuilders()
 testpuremaster()
 @testset "cutting stock - colgen root " begin
@@ -31,3 +33,4 @@ branch_and_bound_bigger_instances()
 @testset "gap - JuMP/MOI modeling" begin
     include("../demos/GeneralizedAssignment_ColGen/run_sgap.jl")
 end
+
