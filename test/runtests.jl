10e71bca88017b45b85057b4c5e5f96751f6667f
Vitor Nesello <vitornesello@gmail.com>
2018-09-26 18:46:26 +0200

    reverting runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,17 +20,17 @@ include("unit_tests/unit_tests.jl")
 # global_logger(ConsoleLogger(stderr, LogLevel(-5)))
 
 unit_tests()
-# testdefaultbuilders()
-# testpuremaster()
-# @testset "cutting stock - colgen root " begin
-#     testcolgenatroot()
-# end
-# @testset "knapsack - branch and bound" begin
-#     branch_and_bound_test_instance()
-# end
-# branch_and_bound_bigger_instances()
-# # moi_wrapper()
-# @testset "gap - JuMP/MOI modeling" begin
-#     include("../demos/GeneralizedAssignment_ColGen/run_sgap.jl")
-# end
+testdefaultbuilders()
+testpuremaster()
+@testset "cutting stock - colgen root " begin
+    testcolgenatroot()
+end
+@testset "knapsack - branch and bound" begin
+    branch_and_bound_test_instance()
+end
+branch_and_bound_bigger_instances()
+# moi_wrapper()
+@testset "gap - JuMP/MOI modeling" begin
+    include("../demos/GeneralizedAssignment_ColGen/run_sgap.jl")
+end
 
