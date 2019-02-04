8a1e42540fdbfa62e1774e8fc62f59c8864718bb
Vitor Nesello <vitornesello@gmail.com>
2019-02-04 10:59:51 +0100

    indentation

--- examples/CuttingStock_SubprobMultiplicity/model_csp.jl
+++ examples/CuttingStock_SubprobMultiplicity/model_csp.jl
@@ -1,37 +1,39 @@
 function model_scsp(d::DataCsp)
-  params = Coluna.Params(use_restricted_master_heur = true)
+    params = Coluna.Params(use_restricted_master_heur = true)
 
-  csp = Model(with_optimizer(Coluna.ColunaModelOptimizer, params = params),
-              bridge_constraints = false)
+    csp = Model(with_optimizer(Coluna.ColunaModelOptimizer, params = params,
+                               master_factory = with_optimizer(GLPK.Optimizer),
+                               pricing_factory = with_optimizer(GLPK.Optimizer)),
+                bridge_constraints=false)
 
-  xub = [ min(d.orders[o].demand, floor(d.stocksheetswidth/d.orders[o].width))
-          for o in 1:d.nborders ]
+    xub = [ min(d.orders[o].demand, floor(d.stocksheetswidth/d.orders[o].width))
+            for o in 1:d.nborders ]
 
-  @variable(csp, 0 <= x[o in 1:d.nborders] <= xub[o])
+    @variable(csp, 0 <= x[o in 1:d.nborders] <= xub[o])
 
-  @variable(csp, y, Bin)
+    @variable(csp, y, Bin)
 
-  @constraint(csp, cov[o in 1:d.nborders], x[o] >= d.orders[o].demand)
+    @constraint(csp, cov[o in 1:d.nborders], x[o] >= d.orders[o].demand)
 
-  @constraint(csp, knp,
-      sum(x[o] * d.orders[o].width for o in 1:d.nborders)
-      - y * d.stocksheetswidth <= 0)
+    @constraint(csp, knp,
+                sum(x[o] * d.orders[o].width for o in 1:d.nborders)
+                - y * d.stocksheetswidth <= 0)
 
-  @objective(csp, Min, y)
+    @objective(csp, Min, y)
 
-  # setting Dantzig Wolfe composition: one subproblem per machine
-  function csp_decomp_func(name, key)
-      if name == :cov
-          return 0
-      else
-          return 1
-      end
-  end
-  Coluna.set_dantzig_wolfe_decompostion(csp, csp_decomp_func)
+    # setting Dantzig Wolfe composition: one subproblem per machine
+    function csp_decomp_func(name, key)
+        if name == :cov
+            return 0
+        else
+            return 1
+        end
+    end
+    Coluna.set_dantzig_wolfe_decompostion(csp, csp_decomp_func)
 
-  # setting pricing cardinality bounds
-  card_bounds_dict = Dict(1 => (0,10))
-  Coluna.set_dantzig_wolfe_cardinality_bounds(csp, card_bounds_dict)
+    # setting pricing cardinality bounds
+    card_bounds_dict = Dict(1 => (0,10))
+    Coluna.set_dantzig_wolfe_cardinality_bounds(csp, card_bounds_dict)
 
-  return (csp, x,  y)
+    return (csp, x,  y)
 end
