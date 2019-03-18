642efc004b1a4019ee7590b4b3c43343610c7030
fD-V <fv@math.u-bordeaux.fr>
2019-03-18 16:09:49 +0100

    membership with mask

--- src/membership.jl
+++ src/membership.jl
@@ -1,45 +1,53 @@
-struct Membership{T <: AbstractVarConstr}
-    member_coef_map::SparseVector{Float64,Int}
+struct Membership{VCT <: AbstractVarConstr}
+    member_coef_map::SparseVector{Float64,Id{VCT}}
 end
 
 function get_coeff(m::Membership{T}, id::Id{T}) where {T <: AbstractVarConstr}
     return m.member_coef_map[id.id]
 end
 
-mutable struct Manager{T <: AbstractVarConstr}
-    active_static_list::SparseVector{Int,Int}
-    active_dynamic_list::SparseVector{Int,Int}
-    unsuitable_static_list::SparseVector{Int,Int}
-    unsuitable_dynamic_list::SparseVector{Int,Int}
+mutable struct Manager{VCT <: AbstractVarConstr}
+    vc_list::SparseVector{VCT,Id{VCT}}
+    active_mask::SparseVector{Bool,Id{VCT}}
+    static_mask::SparseVector{Bool,Id{VCT}}
 end
 
-Manager{T}() where {T <: AbstractVarConstr} = Manager{T}(spzeros(0), spzeros(0), spzeros(0), spzeros(0))
+Manager{T}() where {T <: AbstractVarConstr} = Manager{T}(spzeros(0), spzeros(0), spzeros(0))
 
 function get_list(m::Manager, active::Bool, static::Bool)
     if active
-        static && return m.active_static_list
-        !static && return m.active_dynamic_list
+        static && return m.vc_list[m.active_mask .& m.static_mask]
+        !static && return m.vc_list[m.active_mask .& !m.static_mask]
     end
-    static && return m.unsuitable_static_list
-    return m.unsuitable_dynamic_list
+    static && return  m.vc_list[!m.active_mask .& m.static_mask]
+    return m.vc_list[!m.active_mask .& m.dynamic_mask]
 end
 
-function add_in_manager(m::Manager{T}, elem::T) where {T <: AbstractVarConstr}
-    list = get_list(m, elem.flag == 'd', elem.status == 's')
-    list[elem.uid] = elem.moi_id
+function get_active_list(m::Manager, active::Bool)
+    if active
+        return m.vc_list[m.active_mask]
+    end
+    return m.vc_list[!m.active_mask]
+end
+
+function get_static_list(m::Manager, static::Bool)
+    if static
+        return m.vc_list[m.static_mask]
+    end
+    return m.vc_list[!m.static_mask]
+end
+
+function add_in_manager(m::Manager{T}, elem::T, active::Bool, static::Bool) where {T <: AbstractVarConstr}
+    m.vc_list[elem.uid] = elem
+    active_mask[elem.uid] = active
+    static_mask[elem.uid] = active   
     return
 end
 
 function remove_from_manager(m::Manager{T}, elem::T) where {T <: AbstractVarConstr}
-    list = get_list(m, elem.flag == 'd', elem.status == 's')
-    deleteat!(list, elem.uid)
+    deleteat!(m.vc_list, elem.uid)
+    deleteat!(m.active_mask, elem.uid)
+    deleteat!(m.static_mask, elem.uid)
     return
 end
-
-struct Table{T <: AbstractVarConstr}
-    elem_to_moi_ref::Dict{Any, Any}
-end
-
-Table{T}() where {T <: AbstractVarConstr} = Table{T}(Dict())
-
 
