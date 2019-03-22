fa89778432bb05f01eaf742f76efbabf5086ff7e
fD-V <fv@math.u-bordeaux.fr>
2019-03-22 10:31:11 +0100

    code reveix formulation

--- src/algColGen.jl
+++ src/algColGen.jl
@@ -3,28 +3,29 @@ function update_pricing_prob(alg::AlgToEvalNodeByLagrangianDuality,
 
     @timeit to(alg) "update_pricing_prob"
     begin
-        new_obj = SparseVector{Float64, MoiIndex}()
+        new_obj = SparseVector{Float64, MOI.VariableIndex}()
         alg.pricing_const_obj[pricing_prob] = 0
 
         active = true
         static = true
-        var_members = get_list(pricing_prob.var_manager, actice, static)
+        var_members = get_var_list(pricing_prob, actice, static)
         for (var_uid, var) in var_members
             @logmsg LogLevel(-4) string("$var original cost = ", var.cost)
-            new_obj[var.moi_def.index] = var.cost
+            new_obj[var.index] = get_var_cost(pricing_prob,var_uid)
         end
-        parent_prob = 
-        master_form = pricing_prob.parent.master
-        duals_dict = master.dual_sol.constr_val_map
-        for (constr, dual) in duals_dict
-            @assert (constr isa MasterConstr) || (constr isa MasterBranchConstr)
-            if constr isa ConvexityConstr &&
-                (extended_prob.pricing_convexity_lbs[pricing_prob] == constr ||
-                 extended_prob.pricing_convexity_ubs[pricing_prob] == constr)
-                alg.pricing_const_obj[pricing_prob] -= dual
-                continue
-            end
-            for (var, coef) in constr.subprob_var_coef_map
+       # master_form = pricing_prob.parent.master
+        dual_solution = get_dual_sol(alg)
+        for (constr_uid, dual_val) in dual_solution
+            #@assert (constr isa MasterConstr) || (constr isa MasterBranchConstr)
+            #if constr isa ConvexityConstr &&
+            #    (extended_prob.pricing_convexity_lbs[pricing_prob] == constr ||
+            #     extended_prob.pricing_convexity_ubs[pricing_prob] == constr)
+            #    alg.pricing_const_obj[pricing_prob] -= dual
+            #    continue
+            # end
+            constr_membership = get_var_member(pricing_prob, constr_uid)
+            for (var_uid, coef) in constr_membership
+                if haskey(new_obj, var) 
                 if haskey(new_obj, var)
                     new_obj[var] -= dual * coef
                 end
--- src/formulation.jl
+++ src/formulation.jl
@@ -30,17 +30,34 @@ function add_variable!(m::Memberships, var::Variable)
     return
 end
 
+function add_variable!(m::Memberships,  var_uid::VarId)
+    m.var_memberships[var_uid] = spzeros(Float64, MAX_SV_ENTRIES)
+    return
+end
+
 function add_variables!(m::Memberships, vars::Vector{Variable}, 
         memberships::Vector{SparseVector})
     println("\e[31m register vars membership \e[00m")
 end
 
+# FVR better todefine add_constrait :one at a time
+function add_constraints!(m::Memberships,
+                          constr_uid::ConstrId, 
+                          membership::SparseVector) 
+    m.constr_memberships[constr_uid] = membership
+    var_uids, vals = findnz(membership) # FVR membership should hold only non zeros
+    for j in 1:length(var_uids)
+        m.var_memberships[var_uids[j]][constr_uid] = vals[j]
+    end
+    return
+end
+
 function add_constraints!(m::Memberships, constrs::Vector{Constraint}, 
-        memberships::Vector{SparseVector})
+                          memberships::Vector{SparseVector}) 
     for i in 1:length(constrs)
         constr_uid = getuid(constrs[i])
         m.constr_memberships[constr_uid] = memberships[i]
-        var_uids, vals = findnz(memberships[i])
+        var_uids, vals = findnz(memberships[i]) # FVR memberships[i] should hold only non zeros
         for j in 1:length(var_uids)
             m.var_memberships[var_uids[j]][constr_uid] = vals[j]
         end
@@ -85,20 +102,25 @@ function Formulation(m::AbstractModel, moi::Union{MOI.ModelLike, Nothing})
         nothing, vtypes, csenses, Min)
 end
 
-function register_variables!(f::Formulation, vars::Vector{Variable}, 
-        costs::Vector{Float64}, lb::Vector{Float64}, ub::Vector{Float64}, 
-        vtypes::Vector{VarType})
+function register_variables!(f::Formulation,
+                             vars::Vector{Variable}, 
+                             costs::Vector{Float64},
+                             lb::Vector{Float64},
+                             ub::Vector{Float64}, 
+                             vtypes::Vector{VarType})
     @assert length(vars) == length(costs) == length(ub)
     @assert length(vars) == length(lb) == length(vtypes)
     for i in 1:length(vars)
         uid = getuid(vars[i])
-        add_variable!(f.memberships, vars[i])
-        # register in manager
-        if f.obj_sense == Max
-            f.costs[uid] = -1.0 * costs[i]
-        else
-            f.costs[uid] = costs[i]
-        end
+        add_variable!(f.memberships, uid) # FVR
+        #add_variable!(f.memberships, vars[i])
+        # register in manager #FVR already done in  register_objective_sense!(
+        # if f.obj_sense == Max
+        #     f.costs[uid] = -1.0 * costs[i]
+        # else
+        #     f.costs[uid] = costs[i]
+        # end
+        f.costs[uid] = costs[i]
         f.lower_bounds[uid] = lb[i]
         f.upper_bounds[uid] = ub[i]
         # vtypes
@@ -106,9 +128,11 @@ function register_variables!(f::Formulation, vars::Vector{Variable},
     return
 end
 
-function register_constraints!(f::Formulation, constrs::Vector{Constraint},
-        memberships::Vector{SparseVector}, csenses::Vector{ConstrSense},
-        rhs::Vector{Float64})
+function register_constraints!(f::Formulation,
+                               constrs::Vector{Constraint},
+                               memberships::Vector{SparseVector},
+                               csenses::Vector{ConstrSense},
+                               rhs::Vector{Float64})
     @assert length(constrs) == length(memberships) == length(csenses) == length(rhs)
     # register in manager
     for i in 1:length(constrs)
@@ -119,8 +143,10 @@ function register_constraints!(f::Formulation, constrs::Vector{Constraint},
             memberships[i] *= -1.0
             rhs[i] *= -1.0
         end
+        add_constraints!(f.memberships, uid, memberships[i])
+
     end
-    add_constraints!(f.memberships, constrs, memberships)
+    #   add_constraints!(f.memberships, constrs, memberships) #FVR
     return
 end
 
