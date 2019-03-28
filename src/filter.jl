b95df6b7a6c9bdfb338202981a61f327a8676985
Vitor Nesello <vitornesello@gmail.com>
2019-03-28 22:45:17 +0100

    put back duty_sets for now in manager

--- src/filter.jl
+++ src/filter.jl
@@ -5,14 +5,6 @@ struct Filter{T}
     # Add info to know when to call dropzeros
 end
 
-getmask(f::Filter) = f.mask
-getf(f::Filter) = f.f
-getvm(f::Filter) = f.var_manager
-
-function get_nz_ids(f::Filter)
-    return findnz(f.mask)[1]
-end
-
 function Filter(container::C, f::Function, varconstrs::SparseVector{T,Int}
                 ) where {C,T}
     mask = SparseVector{Bool,Int}(spzeros(Bool, MAX_SV_ENTRIES))
@@ -25,7 +17,15 @@ function Filter(container::C, f::Function, varconstrs::SparseVector{T,Int}
     return Filter(mask, container, f)
 end
 
-function update_filter(filter::Filter, uid::Int)
+getmask(f::Filter) = f.mask
+getf(f::Filter) = f.f
+getvm(f::Filter) = f.var_manager
+
+function get_nz_ids(f::Filter)
+    return findnz(f.mask)[1]
+end
+
+function update_mask(filter::Filter, uid::Int)
     vc = getvc(filter.container, uid)
     if filter.f(vc)
         filter.mask[uid] = true
@@ -36,7 +36,8 @@ function remove_element(filter::Filter, uid::Int)
     filter.mask[uid] = false
 end
 
-function update_mask(filter::Filter)
+function update_mask(filter::Filter, varconstrs::SparseVector{T,Int}
+                     ) where {C,T}
     # This will be useful to update the filter in the begining of an algorithm
     # that does not know what has changed in the container
     # filter.mask = filter.maks .& get_all(filter.container)
--- src/manager.jl
+++ src/manager.jl
@@ -1,16 +1,31 @@
 struct Manager{T <: AbstractVarConstr} <: AbstractManager
     members::SparseVector{T,Int}
+    duty_sets::Dict{Type{<: AbstractDuty}, Vector{Int}}
 end
 
-Manager(T::Type{<:AbstractVarConstr}) = Manager{T}(spzeros(MAX_SV_ENTRIES))
+Manager(T::Type{<:AbstractVarConstr}) = Manager{T}(
+    spzeros(MAX_SV_ENTRIES), Dict{Type{<: AbstractDuty}, Vector{Int}}()
+)
 getvc(m::Manager, uid::Int) = m.members[uid]
 
 function get_nz_ids(m::Manager)
     return findnz(m.members)[1]
 end
 
-function add!(vm::Manager, vc::T) where T <: Union{Variable, Constraint}
+function add!(vcm::Manager, vc::T) where T <: Union{Variable, Constraint}
     uid = getuid(vc)
-    vm.members[uid] = vc
+    vcm.members[uid] = vc
+
+    duty = getduty(vc)
+    if haskey(vcm.duty_sets, duty)
+        set = vcm.duty_sets[duty]
+    else
+        set = vcm.duty_sets[duty] = Vector{Int}()
+    end
+    push!(set, uid)
+
     return
 end
+
+
+
