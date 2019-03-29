f445fdc8251d347a448ab9fca7ec18e1fcbb55fc
Vitor Nesello <vitornesello@gmail.com>
2019-03-29 11:08:35 +0100

    remove duty_sets

--- src/manager.jl
+++ src/manager.jl
@@ -1,10 +1,10 @@
 struct Manager{T <: AbstractVarConstr} <: AbstractManager
     members::SparseVector{T,Int}
-    duty_sets::Dict{Type{<: AbstractDuty}, Vector{Int}}
+    # duty_sets::Dict{Type{<: AbstractDuty}, Vector{Int}}
 end
 
 Manager(T::Type{<:AbstractVarConstr}) = Manager{T}(
-    spzeros(MAX_SV_ENTRIES), Dict{Type{<: AbstractDuty}, Vector{Int}}()
+    spzeros(MAX_SV_ENTRIES)#, Dict{Type{<: AbstractDuty}, Vector{Int}}()
 )
 getvc(m::Manager, uid::Int) = m.members[uid]
 
@@ -16,13 +16,13 @@ function add!(vcm::Manager, vc::T) where T <: Union{Variable, Constraint}
     uid = getuid(vc)
     vcm.members[uid] = vc
 
-    duty = getduty(vc)
-    if haskey(vcm.duty_sets, duty)
-        set = vcm.duty_sets[duty]
-    else
-        set = vcm.duty_sets[duty] = Vector{Int}()
-    end
-    push!(set, uid)
+    # duty = getduty(vc)
+    # if haskey(vcm.duty_sets, duty)
+    #     set = vcm.duty_sets[duty]
+    # else
+    #     set = vcm.duty_sets[duty] = Vector{Int}()
+    # end
+    # push!(set, uid)
 
     return
 end
