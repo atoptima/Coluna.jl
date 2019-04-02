099e1b2cdfc80d0b9406579cdf5196a59f6de958
Guillaume Marques <guillaume.marques@inria.fr>
2019-04-02 14:40:52 +0200

    update perf tests

--- prototype_tests/performance/filter.jl
+++ prototype_tests/performance/filter.jl
@@ -1,12 +1,15 @@
 using BenchmarkTools
 using SparseArrays
 
+using Coluna
+
 abstract type AbstractDuty end
 struct Original <: AbstractDuty end
 struct Pricing <: AbstractDuty end
 struct Implicit <: AbstractDuty end
 struct Another <: AbstractDuty end
 struct LastOne <: AbstractDuty end
+struct Empty <: AbstractDuty end
 
 struct Variable{T <: AbstractDuty}
     id::Int
@@ -15,6 +18,7 @@ struct Variable{T <: AbstractDuty}
     flag::Bool
 end
 
+zero(::Variable) = Variable{Empty}(g)
 getduty(v::Variable{T}) where {T <: AbstractDuty} = T
 
 function init_structs()
@@ -44,6 +48,10 @@ function init_structs()
     return sv, dict
 end
 
+function createfilter(sv, f)
+    return Coluna.Filter(f, sv)
+end
+
 function benchmarks()
     sv, dict = init_structs()
     f1(var) = (var[2].flag == true)
@@ -58,7 +66,47 @@ function benchmarks()
     @show length(d2)
     @btime $(d3 = filter(f3, dict))
     @show length(d3)
+
+    #@btime $(sv )
+    
+
+    #println("------")
+    #@btime $(e1 = sv[f1.(sv)])
+    #@show typeof(e1)
     return
 end
 
-benchmarks()
+
+function init_structs2()
+    ids = Int[]
+    values = Float64[]
+    for i in 1:10_000_000
+        p = rand(0:0.0001:1)
+        if p < 0.5
+            push!(ids, i)
+            push!(values, rand(0:0.001:10))
+        end
+    end
+
+    sv = spzeros(Float64, 10_000_000)
+    dict = Dict{Int, Float64}()
+    for (i, val) in enumerate(values)
+        sv[ids[i]] = values[i]
+        dict[ids[i]] = values[i]
+    end
+    return sv, dict
+end
+
+function multiplication()
+    sv1, dict1 = init_structs2()
+    sv2, dict2 = init_structs2()
+
+    @btime $(dict3 = merge(+, dict1, dict2))
+    @btime $(val1 = mapreduce(e -> e[2], +, dict3))
+    @show val1
+    @btime $(sv3 = sv1 .+ sv2)
+    @btime $(val2 = sum(sv3))
+    @show val2
+end
+
+multiplication()
