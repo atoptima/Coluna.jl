f257ca8096b3e74d01be6729725cac5f5c6e7def
Guillaume Marques <guillaume.marques@inria.fr>
2019-04-10 23:09:48 +0200

    elements in membersvector

--- src/containers/members.jl
+++ src/containers/members.jl
@@ -1,34 +1,36 @@
 abstract type AbstractMembersContainer end
 
-mutable struct MembersVector{I, T} <: AbstractMembersContainer
+mutable struct MembersVector{I,K,T} <: AbstractMembersContainer
+    elements::Dict{I, K}
     members::Dict{I, T}
 end
 
-MembersVector{I,T}() where {I,T} = MembersVector(Dict{I,T}())
+MembersVector{I,K,T}(elems::Dict{I,K}) where {I,K,T} = MembersVector(elems, Dict{I,T}())
 
-Base.eltype(vec::MembersVector{I,T}) where {I,T} = T
+Base.eltype(vec::MembersVector{I,K,T}) where {I,K,T} = T
 Base.ndims(vec::MembersVector) = 1
 
-function Base.setindex!(vec::MembersVector{I,T}, val, id::I) where {I,T}
+function Base.setindex!(vec::MembersVector{I,K,T}, val, id::I) where {I,K,T}
     vec.members[id] = val
 end
 
-function Base.get(vec::MembersVector{I,T}, id::I, default) where {I,T}
+function Base.get(vec::MembersVector{I,K,T}, id::I, default) where {I,K,T}
     Base.get(vec.members, id, default)
 end
 
-function Base.getindex(vec::MembersVector{I,T}, id::I) where {I,T}
+function Base.getindex(vec::MembersVector{I,K,T}, id::I) where {I,K,T}
     vec.members[id]
 end
 
-function Base.getindex(vec::MembersVector{I,T}, id::I) where {I,T<:Number}
+function Base.getindex(vec::MembersVector{I,K,T}, id::I) where {I,K,T<:Number}
     Base.get(vec, id, zero(T))
 end
 
 Base.getindex(vec::MembersVector, ::Colon) = vec
 
-function Base.merge(op, vec1::MembersVector{I,T}, vec2::MembersVector{I,U}) where {I,T,U}
-    MembersVector(Base.merge(op, vec1.members, vec2.members))
+function Base.merge(op, vec1::MembersVector{I,K,T}, vec2::MembersVector{I,K,U}) where {I,K,T,U}
+    (vec1.elements === vec2.elements) || error("elements are not the same.") # too much restrictive ?
+    MembersVector(vec1.elements, Base.merge(op, vec1.members, vec2.members))
 end
 
 function Base.reduce(op, vec::MembersVector)
@@ -39,8 +41,20 @@ function Base.:(==)(vec1::MembersVector, vec2::MembersVector)
     vec1.members == vec2.members
 end
 
+function Base.:(==)(vec1::Dict, vec2::MembersVector)
+    vec1 == vec2.members
+end
+
+function Base.:(==)(vec1::MembersVector, vec2::Dict)
+    vec1.members == vec2
+end
+
 function Base.:(!=)(vec1::MembersVector, vec2::MembersVector)
     vec1.members != vec2.members
+end
+
+function Base.filter(f::Function, vec::MembersVector)
+    MembersVector(vec.elements, Base.filter(e -> f(vec.elements[e[1]]), vec.members))
 end
 
 iterate(d::MembersVector) = iterate(d.members)
@@ -51,21 +65,21 @@ lastindex(d::MembersVector) = lastindex(d.members)
 # =================================================================
 
 struct MembersMatrix{I,K,J,L,T} <: AbstractMembersContainer
-    cols_elements::Dict{I,K}
-    rows_elements::Dict{J,L}
-    cols::Dict{I, MembersVector{J,T}}
-    rows::Dict{J, MembersVector{I,T}}
+    col_elements::Dict{I,K}
+    row_elements::Dict{J,L}
+    cols::Dict{I, MembersVector{J,L,T}}
+    rows::Dict{J, MembersVector{I,K,T}}
 end
 
-function MembersMatrix{I,K,J,L,T}(cols_elems, rows_elems) where {I,K,J,L,T}
-    cols = Dict{I, MembersVector{J,T}}()
-    rows = Dict{J, MembersVector{I,T}}()
-    MembersMatrix(cols_elems, rows_elems, cols, rows)
+function MembersMatrix{I,K,J,L,T}(col_elems, row_elems) where {I,K,J,L,T}
+    cols = Dict{I, MembersVector{J,L,T}}()
+    rows = Dict{J, MembersVector{I,K,T}}()
+    MembersMatrix(col_elems, row_elems, cols, rows)
 end
 
-function _getmembersvector!(dict::Dict{I, MembersVector{J,T}}, key::I) where {I,J,T}
+function _getmembersvector!(dict::Dict{I, MembersVector{J,L,T}}, key::I, elems::Dict{J,L}) where {I,J,L,T}
     if !haskey(dict, key)
-        membersvec = MembersVector{J,T}()
+        membersvec = MembersVector{J,L,T}(elems)
         dict[key] = membersvec
         return membersvec
     end
@@ -73,9 +87,9 @@ function _getmembersvector!(dict::Dict{I, MembersVector{J,T}}, key::I) where {I,
 end
 
 function Base.setindex!(m::MembersMatrix, val, col_id, row_id)
-    cols = _getmembersvector!(m.cols, col_id)
+    cols = _getmembersvector!(m.cols, col_id, m.col_elements)
     cols[row_id] = val
-    rows = _getmembersvector!(m.rows, row_id)
+    rows = _getmembersvector!(m.rows, row_id, m.row_elements)
     rows[col_id] = val
     m
 end
@@ -89,28 +103,28 @@ function Base.getindex(m::MembersMatrix, col_id, row_id)
 end
 
 function Base.getindex(m::MembersMatrix, ::Colon, row_id)
-    _getmembersvector!(m.rows, row_id)
+    _getmembersvector!(m.rows, row_id, m.row_elements)
 end
 
 function Base.getindex(m::MembersMatrix, col_id, ::Colon)
-    _getmembersvector!(m.cols, col_id)
+    _getmembersvector!(m.cols, col_id, m.col_elements)
 end
 
-function setcolumn!(m::MembersMatrix, col_id, new_col::MembersVector)
-    col = deepcopy(new_col)
+function setcolumn!(m::MembersMatrix, col_id, new_col::Dict)
+    col = MembersVector(m.col_elements, deepcopy(new_col))
     m.cols[col_id] = col
     for (row_id, val) in col
-        row = _getmembersvector!(m.rows, row_id)
+        row = _getmembersvector!(m.rows, row_id, m.row_elements)
         row[col_id] = val
     end
     m
 end
 
-function setrow!(m::MembersMatrix, row_id, new_row::MembersVector)
-    row = deepcopy(new_row)
+function setrow!(m::MembersMatrix, row_id, new_row::Dict)
+    row = MembersVector(m.row_elements, deepcopy(new_row))
     m.rows[row_id] = row
     for (col_id, val) in row
-        col = _getmembersvector!(m.cols, col_id)
+        col = _getmembersvector!(m.cols, col_id, m.col_elements)
         col[row_id] = val
     end
     m
--- test/unit/vcdict.jl
+++ test/unit/vcdict.jl
@@ -3,15 +3,17 @@ function vcdict_unit_tests()
 end
 
 function vcdict_base_unit_tests()
-    a = CL.MembersVector{Int, Float64}()
+    elems = Dict(1=>false, 2=> true, 3=> true, 4=>false)
+    a = CL.MembersVector{Int, Bool, Float64}(elems)
     a[1] = 1
     @test a[1] == 1.0
     @test a[2] == 0.0
     @test a[:] == a
 
-    b = CL.MembersVector{Int, Float64}()
+    b = CL.MembersVector{Int, Bool, Float64}(elems)
     b[4] = 2.0
     b[1] = 1.5
+    b[2] = 8.0
 
     sum_id = 0
     sum_val = 0.0
@@ -19,13 +21,13 @@ function vcdict_base_unit_tests()
         sum_id += id
         sum_val += val
     end
-    @test sum_id == 5
-    @test sum_val == 3.5
+    @test sum_id == 7
+    @test sum_val == 11.5
 
     c = merge(+, a, b)
     @test c[1] == 2.5
     d = reduce(+, c)
-    @test d == 4.5
+    @test d == 12.5
 
     cols_elems = Dict(1 => true, 2 => true, 4 => false, 5 => false)
     rows_elems = Dict(1 => true, 2 => false, 3 => false, 6 => true)
@@ -35,7 +37,7 @@ function vcdict_base_unit_tests()
     @test m[1,:][2] == 1.0
     @test m[:,2][1] == 1.0
     
-    new_column = CL.MembersVector{Int, Float64}()
+    new_column = Dict{Int, Float64}()
     new_column[2] = 9.0
     new_column[5] = 2.0
 
@@ -43,5 +45,4 @@ function vcdict_base_unit_tests()
     @test m[2, :] == new_column
     @test m[2, 5] == 2.0
 
-    
 end
