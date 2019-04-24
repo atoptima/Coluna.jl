aea795c7b4be08a76694279a0f714a5c1df55b73
Vitor Nesello <vitornesello@gmail.com>
2019-04-24 17:06:04 +0200

    more tests

--- test/unit/varconstr.jl
+++ test/unit/varconstr.jl
@@ -1,6 +1,94 @@
 function varconstr_unit_tests()
-    varconstr_data_getters_and_setters_tests()
+    abstract_vc_data_getters_and_setters_tests()
+    abstract_var_constr_getters_tests()
+    varcosntr_helpers_tests()
 end
 
-function varconstr_data_getters_and_setters_tests()
+function abstract_vc_data_getters_and_setters_tests()
+
+    v_data = CL.VarData(
+        ;cost = 13.0, lb = -10.0, ub = 100.0, kind = CL.Continuous,
+        inc_val = -135.7, sense = CL.Free, is_active = false, is_explicit = false
+    )
+
+    @test CL.is_active(v_data) == false
+    @test CL.is_explicit(v_data) == false
+    @test CL.get_kind(v_data) == CL.Continuous
+    @test CL.get_sense(v_data) == CL.Free
+    @test CL.get_inc_val(v_data) == -135.7
+
+    CL.set_inc_val!(v_data, 1.0)
+    CL.set_is_active!(v_data, true)
+    CL.set_is_explicit!(v_data, true)
+    CL.set_kind!(v_data, CL.Integ)
+    CL.set_sense!(v_data, CL.Negative)
+
+    @test CL.is_active(v_data) == true
+    @test CL.is_explicit(v_data) == true
+    @test CL.get_kind(v_data) == CL.Integ
+    @test CL.get_sense(v_data) == CL.Negative
+    @test CL.get_inc_val(v_data) == 1.0
+
+
+    c_data = CL.ConstrData(
+        ; rhs = -13.0, kind = CL.Facultative, sense = CL.Equal,
+        inc_val = -12.0, is_active = false, is_explicit = false
+    )
+
+    @test CL.is_active(c_data) == false
+    @test CL.is_explicit(c_data) == false
+    @test CL.get_kind(c_data) == CL.Facultative
+    @test CL.get_sense(c_data) == CL.Equal
+    @test CL.get_inc_val(c_data) == -12.0
+
+    CL.set_inc_val!(c_data, 1.0)
+    CL.set_is_active!(c_data, true)
+    CL.set_is_explicit!(c_data, true)
+    CL.set_kind!(c_data, CL.Core)
+    CL.set_sense!(c_data, CL.Less)
+
+    @test CL.is_active(c_data) == true
+    @test CL.is_explicit(c_data) == true
+    @test CL.get_kind(c_data) == CL.Core
+    @test CL.get_sense(c_data) == CL.Less
+    @test CL.get_inc_val(c_data) == 1.0
+
+end
+
+function abstract_var_constr_getters_tests()
+
+    v = CL.Variable(
+        CL.Id{CL.Variable}(23, 10), "fake_var", CL.MastRepBendSpVar
+    )
+
+    @test CL.get_id(v) == CL.Id{CL.Variable}(23, 10)
+    @test CL.get_name(v) == "fake_var"
+    @test CL.get_duty(v) == CL.MastRepBendSpVar
+    @test CL.get_initial_data(v) === v.initial_data
+    @test CL.get_cur_data(v) === v.cur_data
+    @test CL.get_moi_record(v) === v.moi_record
+
+
+    c = CL.Constraint(
+        CL.Id{CL.Constraint}(23, 10), "fake_constr", CL.MasterBranchConstr
+    )
+
+    @test CL.get_id(c) == CL.Id{CL.Constraint}(23, 10)
+    @test CL.get_name(c) == "fake_constr"
+    @test CL.get_duty(c) == CL.MasterBranchConstr
+    @test CL.get_initial_data(c) === c.initial_data
+    @test CL.get_cur_data(c) === c.cur_data
+    @test CL.get_moi_record(c) === c.moi_record
+end
+
+function varcosntr_helpers_tests()
+
+    v = CL.Variable(
+        CL.Id{CL.Variable}(23, 10), "fake_var", CL.MastRepBendSpVar
+    )
+
+    CL.get_uid(v) == 23
+    CL.get_form(v) == 10
+
+
 end
