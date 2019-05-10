e33a050bf83708af694a7ac6a910cc63c15a20e7
Guillaume Marques <guillaume.marques@protonmail.com>
2019-05-10 10:59:58 +0200

    typo

--- src/strategies/divide/strategy.jl
+++ src/strategies/divide/strategy.jl
@@ -2,5 +2,5 @@ abstract type AbstractDivideStrategy <: AbstractStrategy end
 
 function apply!(S::Type{<:AbstractDivideStrategy}, reform, node, 
                 strategy_rec::StrategyRecord, params)
-    error("Method apply! not implemented for branching strategy $S.")
+    error("Method apply! not implemented for divide strategy $S.")
 end
