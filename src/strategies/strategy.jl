7929c937bb33d3baf6252f47c9b771ae6f10c082
Guillaume Marques <guillaume.marques@protonmail.com>
2019-05-11 10:16:08 +0200

    replace interface by prepare

--- src/strategies/strategy.jl
+++ src/strategies/strategy.jl
@@ -2,8 +2,10 @@ abstract type AbstractStrategy end
 
 mutable struct StrategyRecord
     cur_solver::Type{<:AbstractSolver}
+    ext::Dict{Symbol, Any}
 end
 
-StrategyRecord() = StrategyRecord(StartNode)
+StrategyRecord() = StrategyRecord(StartNode, Dict{Symbol, Any}())
+
 setsolver!(r::StrategyRecord, s::Type{<:AbstractSolver}) = r.cur_solver = s
 getsolver(r::StrategyRecord) = r.cur_solver
