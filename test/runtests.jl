e999180708cc000725d6ba4cb95d7676064db570
Guillaume Marques <guillaume.marques@protonmail.com>
2019-05-20 18:06:09 +0200

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,20 @@ include("unit/unit_tests.jl")
 
 unit_tests()
 
+@testset "play gap" begin
+    data = CLD.GeneralizedAssignment.data("play2.txt")
+
+    coluna = JuMP.with_optimizer(Coluna.Optimizer,# #params = params,
+        master_factory = with_optimizer(GLPK.Optimizer),
+        pricing_factory = with_optimizer(GLPK.Optimizer))
+
+    problem, x, dec = CLD.GeneralizedAssignment.model(data, coluna)
+
+    JuMP.optimize!(problem)
+    abs(JuMP.objective_value(problem) - 75.0) <= 0.00001
+    CLD.GeneralizedAssignment.print_and_check_sol(data, problem, x)
+end
+
 @testset "gap - JuMP/MOI modeling" begin
     data = CLD.GeneralizedAssignment.data("smallgap3.txt")
 
@@ -26,7 +40,8 @@ unit_tests()
     problem, x, dec = CLD.GeneralizedAssignment.model(data, coluna)
 
     JuMP.optimize!(problem)
-    #@show JuMP.objective_value(problem)
+    abs(JuMP.objective_value(problem) - 438.0) <= 0.00001
+    CLD.GeneralizedAssignment.print_and_check_sol(data, problem, x)
 end
 
 @testset "gap with penalties - pure master variables" begin
@@ -39,7 +54,7 @@ end
 
     problem, x, y, dec = CLD.GeneralizedAssignment.model_with_penalties(data, coluna)
     JuMP.optimize!(problem)
-    #@test JuMP.objective_value(problem) == 416.4
+    @test abs(JuMP.objective_value(problem) - 416.4) <= 0.00001
 end
 
 @testset "gap with maximisation objective function" begin
@@ -52,5 +67,5 @@ end
 
     problem, x, dec = CLD.GeneralizedAssignment.model_max(data, coluna)
     JuMP.optimize!(problem)
-    #@test JuMP.objective_value(problem) ==  416.4 ?
+    @test abs(JuMP.objective_value(problem) - 580.0) <= 0.00001
 end
