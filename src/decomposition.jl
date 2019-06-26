c57807aa98c4b3cd17b80a50a634cbc3dfb006bb
Guillaume Marques <guillaume.marques@protonmail.com>
2019-06-26 12:07:17 +0200

    rm representatives in rhs

--- src/decomposition.jl
+++ src/decomposition.jl
@@ -214,13 +214,12 @@ function create_side_vars_constrs!(mast::Formulation{BendersMaster}, orig_form::
             lb = -1.0, ub = Inf, 
             kind = Continuous, sense = Free, is_explicit = true, id = getid(nu)
         )
-        setvar!(
-            sp, name, BendSpRepSecondStageCostVar; cost = getcurcost(nu),
-            lb = -Inf, ub = Inf, 
-            kind = Continuous, sense = Free, is_explicit = false,
-            id = Id{Variable}(getid(nu), 2)
-        )
-        
+        # setvar!(
+        #     sp, name, BendSpRepSecondStageCostVar; cost = getcurcost(nu),
+        #     lb = -Inf, ub = Inf, 
+        #     kind = Continuous, sense = Free, is_explicit = false,
+        #     id = Id{Variable}(getid(nu), 2)
+        # )
         # clone_in_formulation!(mast, sp, eta, MasterBendSecondStageCostVar)
     end
     return
@@ -262,12 +261,12 @@ function instantiate_orig_vars!(sp::Formulation{BendersSp}, orig_form::Formulati
                     lb = -Inf, ub = Inf, 
                     kind = Continuous, sense = Free, is_explicit = true, id = id
                 )
-                setvar!(
-                    sp, getname(var), BendSpRepFirstStageVar; cost = 0.0,
-                    lb = -Inf, ub = Inf, 
-                    kind = getperenekind(var), sense = getperenesense(var), is_explicit = false,
-                    id = Id{Variable}(getid(mu), 2)
-                )
+                # setvar!(
+                #     sp, getname(var), BendSpRepFirstStageVar; cost = 0.0,
+                #     lb = -Inf, ub = Inf, 
+                #     kind = getperenekind(var), sense = getperenesense(var), is_explicit = false,
+                #     id = Id{Variable}(getid(mu), 2)
+                # )
             end
         end
     end
