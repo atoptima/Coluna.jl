bf6324e25d996eb4e9f2ebe13924e2e0dcaf33cc
Guillaume Marques <guillaume.marques@protonmail.com>
2019-09-16 20:01:28 -0300

    inline

--- src/containers/elements.jl
+++ src/containers/elements.jl
@@ -2,30 +2,12 @@ struct ElemDict{VC <: AbstractVarConstr}
     elements::Dict{Id{VC}, VC}
 end
 
-function ElemDict{VC}() where {VC <: AbstractVarConstr}
-    return ElemDict{VC}(Dict{Id{VC}, VC}())
-end
-
-function Base.setindex!(d::ElemDict{VC}, val::VC, id::Id{VC}) where {VC}
-    d.elements[id] = val
-end
-
-function Base.getindex(d::ElemDict{VC}, id::Id{VC}) where {VC}
-    Base.getindex(d.elements, id)
-end
-
-function Base.haskey(d::ElemDict{VC}, id::Id{VC}) where {VC}
-    Base.haskey(d.elements, id)
-end
-
-function Base.keys(d::ElemDict)
-    Base.keys(d.elements)
-end
-
-function Base.values(d::ElemDict)
-    Base.values(d.elements)
-end
-
+ElemDict{VC}() where {VC <: AbstractVarConstr} = ElemDict{VC}(Dict{Id{VC}, VC}())
+Base.setindex!(d::ElemDict{VC}, val::VC, id::Id{VC}) where {VC} = d.elements[id] = val
+Base.getindex(d::ElemDict{VC}, i::Id{VC}) where {VC} = Base.getindex(d.elements, i)
+Base.haskey(d::ElemDict{VC}, i::Id{VC}) where {VC} = Base.haskey(d.elements, i)
+Base.keys(d::ElemDict) = Base.keys(d.elements)
+Base.values(d::ElemDict) = Base.values(d.elements)
 iterate(d::ElemDict) = iterate(d.elements)
 iterate(d::ElemDict, state) = iterate(d.elements, state)
 length(d::ElemDict) = length(d.elements)
