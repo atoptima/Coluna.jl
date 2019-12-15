0ba9c56756980137bcf9e597df57c33e7df62848
Guillaume Marques <guillaume.marques@protonmail.com>
2019-12-15 22:53:35 +0100

    fix pricing callback test

--- test/pricing_callback_tests.jl
+++ test/pricing_callback_tests.jl
@@ -36,8 +36,8 @@ function pricing_callback_tests()
         coluna = JuMP.with_optimizer(CL.Optimizer,
             default_optimizer = with_optimizer(
             GLPK.Optimizer), params = CL.Params(
-                ;global_strategy = CL.GlobalStrategy(CL.BnPnPreprocess(),
-                CL.NoBranching(), CL.DepthFirst())
+                ;global_strategy = CL.GlobalStrategy(CL.SimpleBnP(),
+                CL.SimpleBranching(), CL.DepthFirst())
             )
         )
 
