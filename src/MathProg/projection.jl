cc99cbd404fc94053e221bdcaafc8c1324d103f4
Guillaume Marques <guillaume.marques@protonmail.com>
2020-02-21 01:24:49 +0100

    Fix projection of dw reformulation to original formulation (#253)
    * fix proj of master pure var
    
    * MOI 0.9.9
    
    * try to fix MOI0.9.9
    
    * try again
    
    * try again again
    
    * rm modifs in Manifest & Project
    
    * forgot vers of BlockDecomposition

--- src/MathProg/projection.jl
+++ src/MathProg/projection.jl
@@ -1,22 +1,23 @@
 projection_is_possible(master::Formulation{DwMaster}) = true
-
 function proj_cols_on_rep(sol::PrimalSolution{Sense}, master::Formulation{DwMaster}) where {Sense}
     projected_sol_vars = Vector{VarId}()
     projected_sol_vals = Vector{Float64}()
-    for (mc_id, mc_val) in sol
-        origin_form_uid = getoriginformuid(mc_id)
-        # TODO : enhance following
-        spform = master 
-        if origin_form_uid != 0 && origin_form_uid != 1 # if id = 1 this is the master
+
+    for (varid, val) in sol
+        duty = getduty(getvar(master, varid))
+        if duty <= MasterPureVar
+            push!(projected_sol_vars, varid)
+            push!(projected_sol_vals, val)
+        elseif duty <= MasterCol
+            origin_form_uid = getoriginformuid(varid)
             spform = get_dw_pricing_sps(master.parent_formulation)[origin_form_uid]
-        end
-        # END TODO
-        col = getprimalsolmatrix(spform)[:, mc_id]
-        for (rep_id, rep_val) in Iterators.filter(
-            v -> getduty(v) <= DwSpPricingVar || getduty(v) <= DwSpSetupVar,
-            col)
-            push!(projected_sol_vars, rep_id)
-            push!(projected_sol_vals, rep_val * mc_val)
+            col = getprimalsolmatrix(spform)[:, varid]
+            for (repid, repval) in Iterators.filter(
+                    v -> getduty(v) <= DwSpPricingVar || getduty(v) <= DwSpSetupVar,
+                col)
+                push!(projected_sol_vars, repid)
+                push!(projected_sol_vals, repval * val)
+            end
         end
     end
     return PrimalSolution(master, projected_sol_vars, projected_sol_vals, float(getbound(sol)))
