0d9fa31ea53c5d2afe32e379ebbdb80d82a22b37
Guillaume Marques <guillaume.marques@protonmail.com>
2020-03-15 19:01:40 +0100

    Update readme (#282)
    * update1
    
    * v2
    
    * img
    
    * doc
    
    * update Readme
    
    * update remerciements
    
    * update coluna header
    
    * update readme

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,7 +1,7 @@
 function _welcome_message()
     welcome = """
     Coluna
-    Version 0.2 - https://github.com/atoptima/Coluna.jl
+    Version 0.3 - https://github.com/atoptima/Coluna.jl
     """
     print(welcome)
 end
