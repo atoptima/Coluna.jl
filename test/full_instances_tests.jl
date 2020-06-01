b12a1f41d0f483d188ab88b1efc31511fecf7cae
Guillaume Marques <guillaume.marques@protonmail.com>
2020-06-01 15:05:36 +0200

    decomment tests

--- test/full_instances_tests.jl
+++ test/full_instances_tests.jl
@@ -1,11 +1,11 @@
 using LightGraphs
 
 function full_instances_tests()
-    #generalized_assignment_tests()
-    #capacitated_lot_sizing_tests()
-    #lot_sizing_tests()
+    generalized_assignment_tests()
+    capacitated_lot_sizing_tests()
+    lot_sizing_tests()
     ##facility_location_tests()
-    #cutting_stock_tests()
+    cutting_stock_tests()
     cvrp_tests()
 end
 
