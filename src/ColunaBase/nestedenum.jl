1f7ef86ef479ae2d81ad4c2060c910a5123161ef
Guillaume Marques <guillaume.marques@protonmail.com>
2020-11-08 13:11:50 +0100

    whitespace

--- src/ColunaBase/nestedenum.jl
+++ src/ColunaBase/nestedenum.jl
@@ -144,7 +144,7 @@ Create a `NestedEnum` subtype such as :
 # Example
 
 ```jldoctest nestedexample
-julia > Coluna.ColunaBase.@nestedenum begin 
+julia> Coluna.ColunaBase.@nestedenum begin 
     TypeOfItem
     ItemA <= TypeOfItem
         ChildA1 <= ItemA
