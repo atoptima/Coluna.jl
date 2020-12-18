0ceafb6dd7d5d4cc181db60aa3f62457a1741beb
Guillaume Marques <guillaume.marques@protonmail.com>
2020-12-18 19:56:01 +0100

    Rm unused Params

--- src/parameters.jl
+++ src/parameters.jl
@@ -1,8 +1,6 @@
 @with_kw mutable struct Params
     tol::Float64 = 1e-8 # if - ϵ_tol < val < ϵ_tol, we consider val = 0
     tol_digits::Int = 8 # because round(val, digits = n) where n is from 1e-n
-    cut_up::Float64 = Inf
-    cut_lo::Float64 = -Inf
     global_art_var_cost::Union{Float64, Nothing} = nothing
     local_art_var_cost::Union{Float64, Nothing} = nothing
     force_copy_names::Bool = false
