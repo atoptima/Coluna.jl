00a6d036cc24cecd32ab4586b53547837eae67c0
Guillaume Marques <guillaume.marques@protonmail.com>
2020-12-29 12:44:10 +0100

    fix

--- src/parameters.jl
+++ src/parameters.jl
@@ -1,13 +1,12 @@
 """
     Coluna.Params(
-        "solver" => GLPK.Optimizer,
-        "global_art_var_cost" => 10e6,
-        "local_art_var_cost" => 10e4
+        solver = Coluna.Algorithm.TreeSearchAlgorithm(),
+        global_art_var_cost = 10e6,
+        local_art_var_cost = 10e4
     )
 
 Parameters of Coluna :
-- `solver` is the underlying MILP solver used to optimize the restricted master problem
-  and the subproblems
+- `solver` is the algorithm used to optimize the reformulation.
 - `global_art_var_cost` is the cost of the global artificial variables in the master
 - `local_art_var_cost` is the cost of the local artificial variables in the master
 """
