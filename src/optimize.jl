c2e0b0f6708b1c18a7136729d96ca3e4a1408ebb
laradicp <laradicp@gmail.com>
2021-03-08 12:28:42 -0300

    optimize twice 3/4 cases

--- src/optimize.jl
+++ src/optimize.jl
@@ -42,6 +42,9 @@ Starting point of the solver.
 function optimize!(prob::MathProg.Problem, annotations::Annotations, params::Params)
     _welcome_message()
 
+    buffer_reset = prob.original_formulation.buffer
+    ann_pf_reset = annotations.ann_per_form
+
     # Adjust parameters
     ## Retrieve initial bounds on the objective given by the user
     init_pb = get_initial_primal_bound(prob)
@@ -53,11 +56,6 @@ function optimize!(prob::MathProg.Problem, annotations::Annotations, params::Par
     # Apply decomposition
     reformulate!(prob, annotations, env)
     
-    if prob.re_formulation != nothing
-        @show prob.re_formulation.master
-        @show prob.re_formulation.master.optimizer.inner
-    end
-    
     # Coluna ready to start
     set_optim_start_time!(env)
     @logmsg LogLevel(-1) "Coluna ready to start."
@@ -68,8 +66,8 @@ function optimize!(prob::MathProg.Problem, annotations::Annotations, params::Par
     end
 
     env.kpis.elapsed_optimization_time = elapsed_optim_time(env)
-    prob.re_formulation = nothing
-    prob.form_counter = Counter(0)
+    prob.original_formulation.buffer = buffer_reset
+    annotations.ann_per_form = ann_pf_reset
 
     println(_to)
     TO.reset_timer!(_to)
