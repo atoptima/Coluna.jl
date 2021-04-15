f8bbaafc0a366a2572a4d86ea15fb8364c99bfd5
Guillaume Marques <guillaume.marques@protonmail.com>
2021-04-15 17:17:45 +0200

    v0.3.8 (#504)

--- src/optimize.jl
+++ src/optimize.jl
@@ -1,7 +1,7 @@
 function _welcome_message()
     welcome = """
     Coluna
-    Version 0.3.7 | 2021-03-29 | https://github.com/atoptima/Coluna.jl
+    Version 0.3.8 | 2021-04-15 | https://github.com/atoptima/Coluna.jl
     """
     print(welcome)
 end
