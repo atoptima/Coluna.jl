2d61a0ee9866158c322471031413758377913bc4
Guillaume Marques <guillaume.marques@protonmail.com>
2021-08-12 19:20:08 +0200

    Fix support of duplicated terms (#586)
    * Tests pass for duplicated terms
    
    * fix duplicated terms in MOI3
    
    * duplicated terms in objective

--- test/MathOptInterface/MOI_wrapper.jl
+++ test/MathOptInterface/MOI_wrapper.jl
@@ -131,8 +131,6 @@ const UNSUPPORTED_TESTS = [
     "silent", # TODO : support of MOI.Silent()
     "time_limit_sec", # TODO : support of MOI.TimeLimitSec()
     "solve_unbounded_model", # default lower bound 0
-    "solve_duplicate_terms_obj", # TODO: support duplicate terms
-    "solve_duplicate_terms_scalar_affine" # TODO: support duplicate terms
 ]
 
 MathOptInterface.Test.getconstraint
@@ -244,7 +242,6 @@ end
     MOIT.contlineartest(BRIDGED, CONFIG, vcat(
         CONSTRAINTDUAL_SINGLEVAR, MODIFY_DELETE, UNCOVERED_TERMINATION_STATUS, SET_CONSTRAINTSET, [
             "partial_start", # VariablePrimalStart not supported
-            "linear1", # TODO: support duplicate terms
             "linear10" # BUG: optimize twice changing sense from max to min fails
         ]
     ))
