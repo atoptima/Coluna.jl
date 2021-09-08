808b47d043bb1242faac3f93b867d6c7aec57715
Guillaume Marques <guillaume.marques@protonmail.com>
2021-09-08 16:11:25 +0200

    dual bound of tree search is dual bound of open nodes (#598)
    * dual bound of tree search is dual bound of open nodes
    
    * I screwed up the merge

--- src/Algorithm/treesearch.jl
+++ src/Algorithm/treesearch.jl
@@ -75,19 +75,15 @@ push!(tree::SearchTree, node::Node) = DS.enqueue!(tree.nodes, node, getnodevalue
 popnode!(tree::SearchTree) = DS.dequeue!(tree.nodes)
 nb_open_nodes(tree::SearchTree) = length(tree.nodes)
 
-"""
-Data used by the tree search algorithm while running. Destroyed after each run.     
-"""
-mutable struct TreeSearchRuntimeData{Sense}
+"Data used by the tree search algorithm while running. Destroyed after each run."
+mutable struct TreeSearchRuntimeData
     primary_tree::SearchTree
     max_primary_tree_size::Int64
     secondary_tree::SearchTree
     tree_order::Int64
     optstate::OptimizationState
     exploitsprimalsolutions::Bool
-    Sense::Type{<:Coluna.AbstractSense}
     conquer_units_to_restore::UnitsUsage
-    worst_db_of_pruned_node::DualBound{Sense}
 end
 
 function TreeSearchRuntimeData(algo::TreeSearchAlgorithm, reform::Reformulation, input::OptimizationInput)
@@ -100,12 +96,9 @@ function TreeSearchRuntimeData(algo::TreeSearchAlgorithm, reform::Reformulation,
     collect_units_to_restore!(conquer_units_to_restore, algo.conqueralg, reform) 
     # divide algorithms are always manager algorithms, so we do not need to restore storage units for them
 
-    Sense = getobjsense(reform)
-
-    tsdata = TreeSearchRuntimeData{Sense}(
+    tsdata = TreeSearchRuntimeData(
         SearchTree(algo.explorestrategy), algo.opennodeslimit, SearchTree(DepthFirstStrategy()),
-        1, treestate, exploitsprimalsols, Sense, conquer_units_to_restore,
-        -DualBound{Sense}()
+        1, treestate, exploitsprimalsols, conquer_units_to_restore
     )
     master = getmaster(reform)
     push!(tsdata, RootNode(master, getoptstate(input), store_records!(reform), algo.skiprootnodeconquer))
@@ -305,10 +298,9 @@ function run_divide_algorithm!(
     return
 end
 
-function updatedualbound!(data::TreeSearchRuntimeData)
+function updatedualbound!(data::TreeSearchRuntimeData, reform::Reformulation)
     treestate = getoptstate(data)
-    bound_value = getvalue(get_ip_primal_bound(treestate))
-    worst_bound = DualBound{data.Sense}(bound_value)
+    worst_bound = DualBound(reform, getvalue(get_ip_primal_bound(treestate)))
     for (node, _) in getnodes(data.primary_tree)
         db = get_ip_dual_bound(getoptstate(node))
         if isbetter(worst_bound, db)
@@ -321,10 +313,6 @@ function updatedualbound!(data::TreeSearchRuntimeData)
         if isbetter(worst_bound, db)
             worst_bound = db
         end
-    end
-
-    if isbetter(worst_bound, data.worst_db_of_pruned_node)
-        worst_bound = data.worst_db_of_pruned_node
     end
 
     set_ip_dual_bound!(treestate, worst_bound)
@@ -349,15 +337,11 @@ function run!(
         if nodestatus == OPTIMAL || nodestatus == INFEASIBLE ||
            ip_gap_closed(node.optstate, rtol = algo.opt_rtol, atol = algo.opt_atol)             
             println("Node is already conquered. No children will be generated.")
-            db = get_ip_dual_bound(node.optstate)
-            if isbetter(tsdata.worst_db_of_pruned_node, db)
-                tsdata.worst_db_of_pruned_node = db
-            end
         elseif nodestatus != TIME_LIMIT
             run_divide_algorithm!(algo, env, tsdata, reform, node)
         end
 
-        updatedualbound!(tsdata)
+        updatedualbound!(tsdata, reform)
 
         remove_records!(node.recordids)
         # we delete solutions from the node optimization state, as they are not needed anymore
