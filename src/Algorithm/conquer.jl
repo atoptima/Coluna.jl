832f6a97d55ff13f34311f08a540f1652f1108e0
Artur Alves Pessoa <artur.a.pessoa@gmail.com>
2021-09-29 11:14:02 -0300

    Another try to fix the short-circuit problem

--- src/Algorithm/conquer.jl
+++ src/Algorithm/conquer.jl
@@ -293,11 +293,11 @@ function run!(algo::ColCutGenConquer, env::Env, reform::Reformulation, input::Co
         end
 
         # if the gap is still unclosed, try to run the node finalizer if any
-        run_node_finalizer = algo.node_finalizer !== nothing
+        run_node_finalizer = (algo.node_finalizer !== nothing)
         run_node_finalizer = run_node_finalizer && getterminationstatus(nodestate) != TIME_LIMIT
         run_node_finalizer =
             run_node_finalizer && !ip_gap_closed(nodestate, atol = algo.opt_atol, rtol = algo.opt_rtol)
-        run_node_finalizer = run_node_finalizer && getdepth(node) >= algo.node_finalizer.min_depth
+        run_node_finalizer = run_node_finalizer ? getdepth(node) >= algo.node_finalizer.min_depth : false
         run_node_finalizer =
             run_node_finalizer && mod(get_tree_order(node) - 1, algo.node_finalizer.frequency) == 0
 
