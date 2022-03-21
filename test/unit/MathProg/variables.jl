7cae1dcf7bb98df5e0528a37d9f758376468601d
Guillaume Marques <guillaume.marques@protonmail.com>
2022-03-21 12:23:51 +0100

    Single mode multi item lot sizing demo pass

--- test/unit/MathProg/variables.jl
+++ test/unit/MathProg/variables.jl
@@ -44,6 +44,86 @@
         @test ClMP.getcurincval(form, varid) == 4.0
     end
 
+    @testset "bounds of binary variable 1" begin
+        form = ClMP.create_formulation!(Env(Coluna.Params()), ClMP.Original())
+        var = ClMP.setvar!(
+            form, "var1", ClMP.OriginalVar, kind = ClMP.Binary
+        )
+
+        @test ClMP.getperenlb(form, var) == 0.0
+        @test ClMP.getperenub(form, var) == 1.0
+        @test ClMP.getcurlb(form, var) == 0.0
+        @test ClMP.getcurub(form, var) == 1.0
+
+        ClMP.setperenlb!(form, var, -1.0)
+        ClMP.setperenub!(form, var, 2.0)
+
+        @test ClMP.getperenlb(form, var) == 0.0
+        @test ClMP.getperenub(form, var) == 1.0
+        @test ClMP.getcurlb(form, var) == 0.0
+        @test ClMP.getcurub(form, var) == 1.0
+
+        ClMP.setcurlb!(form, var, -1.1)
+        ClMP.setcurub!(form, var, 2.1)
+
+        @test ClMP.getcurlb(form, var) == 0.0
+        @test ClMP.getcurub(form, var) == 1.0
+
+        ClMP.setperenlb!(form, var, 0.1)
+        ClMP.setperenub!(form, var, 0.9)
+
+        @test ClMP.getperenlb(form, var) == 0.1
+        @test ClMP.getperenub(form, var) == 0.9
+        @test ClMP.getcurlb(form, var) == 0.1
+        @test ClMP.getcurub(form, var) == 0.9
+
+        ClMP.setcurlb!(form, var, 0.2)
+        ClMP.setcurub!(form, var, 0.8)
+
+        @test ClMP.getperenlb(form, var) == 0.1
+        @test ClMP.getperenub(form, var) == 0.9
+        @test ClMP.getcurlb(form, var) == 0.2
+        @test ClMP.getcurub(form, var) == 0.8
+    end
+
+    @testset "bounds of binary variable 2" begin
+        form = ClMP.create_formulation!(Env(Coluna.Params()), ClMP.Original())
+        var = ClMP.setvar!(
+            form, "var1", ClMP.OriginalVar, kind = ClMP.Continuous, lb = -10.0, ub = 10.0
+        )
+
+        @test ClMP.getperenlb(form, var) == -10.0
+        @test ClMP.getperenub(form, var) == 10.0
+        @test ClMP.getcurlb(form, var) == -10.0
+        @test ClMP.getcurub(form, var) == 10.0
+
+        ClMP.setperenkind!(form, var, ClMP.Binary)
+
+        @test ClMP.getperenlb(form, var) == 0.0
+        @test ClMP.getperenub(form, var) == 1.0
+        @test ClMP.getcurlb(form, var) == 0.0
+        @test ClMP.getcurub(form, var) == 1.0
+    end
+
+    @testset "bounds of binary variable 3" begin
+        form = ClMP.create_formulation!(Env(Coluna.Params()), ClMP.Original())
+        var = ClMP.setvar!(
+            form, "var1", ClMP.OriginalVar, kind = ClMP.Continuous, lb = -10.0, ub = 10.0
+        )
+
+        @test ClMP.getperenlb(form, var) == -10.0
+        @test ClMP.getperenub(form, var) == 10.0
+        @test ClMP.getcurlb(form, var) == -10.0
+        @test ClMP.getcurub(form, var) == 10.0
+
+        ClMP.setcurkind!(form, var, ClMP.Binary)
+
+        @test ClMP.getperenlb(form, var) == -10.0
+        @test ClMP.getperenub(form, var) == 10.0
+        @test ClMP.getcurlb(form, var) == 0.0
+        @test ClMP.getcurub(form, var) == 1.0
+    end
+
     @testset "record" begin
         v_rec = ClMP.MoiVarRecord(; index = ClMP.MoiVarIndex(-15))
 
