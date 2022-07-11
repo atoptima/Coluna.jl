c3d011aabe1517dc995d622bb9de3946f148a1fb
Guillaume Marques <guillaume.marques@protonmail.com>
2022-07-11 16:59:22 +0200

    ok

--- test/MathOptInterface/MOI_wrapper.jl
+++ test/MathOptInterface/MOI_wrapper.jl
@@ -77,8 +77,7 @@ function test_runtests()
             "test_linear_integration_2",
             "test_linear_integration_Interval",
             "test_linear_integration_delete_variables",
-            "test_linear_transform",
-            "test_cpsat_"
+            "test_linear_transform"
         ],
         # This argument is useful to prevent tests from failing on future
         # releases of MOI that add new tests. Don't let this number get too far
