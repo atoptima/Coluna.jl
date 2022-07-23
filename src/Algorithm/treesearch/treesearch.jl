ef20c159f3404becb53b37a067e42dcbb658ed37
Guillaume Marques <guillaume.marques@protonmail.com>
2022-07-23 22:58:40 +0200

    Tree search interface doc + finish test (#671)

--- src/Algorithm/treesearch/treesearch.jl
+++ src/Algorithm/treesearch/treesearch.jl
@@ -6,7 +6,6 @@ end
 
 # Each conquer algorithm must have a search space.
 function run!(algo::NewTreeSearchAlgorithm, env, reform, input)
-    println("inside run!")
     conquer_space = new_space(algo.conqueralg, env, reform, input)
     tree_search(algo.explorestrategy, algo.conqueralg, algo.dividealg, conquer_space, env)
 end
