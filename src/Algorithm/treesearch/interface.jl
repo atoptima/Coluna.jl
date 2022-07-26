e8a03c6510bac9e1fdb0633dca0c11b3ffd05f95
Guillaume Marques <guillaume.marques@protonmail.com>
2022-07-26 19:00:09 +0200

    rm tracker, rm tree_search dep to conquer/divide (#673)

--- src/Algorithm/treesearch/interface.jl
+++ src/Algorithm/treesearch/interface.jl
@@ -1,7 +1,25 @@
-# The definition of a tree search algorithm is based on four concepts.
+# The definition of a tree search algorithm is based on three concepts.
 
 "Contains the definition of the problem tackled by the tree search algorithm."
 abstract type AbstractSearchSpace end
+
+# Follwing methods will be needed in Coluna only (I'll create a specific abstract subspace for Coluna).
+function get_reformulation(s::AbstractSearchSpace)
+  @warn "get_reformulation(::$(typeof(s))) not implemented."
+  return nothing
+end
+
+function get_conquer(sp::AbstractSearchSpace)
+  @warn "get_conquer(::$(typeof(sp))) not implemented."
+  return nothing
+end
+
+function get_divide(sp::AbstractSearchSpace)
+  @warn "get_divide(::$(typeof(sp))) not implemented."
+  return nothing
+end
+
+tree_search_output(::AbstractSearchSpace) = nothing
 
 "Algorithm that chooses next node to evaluated in the tree search algorithm."
 abstract type AbstractExploreStrategy end
@@ -9,22 +27,29 @@ abstract type AbstractExploreStrategy end
 "A subspace obtained by successive divisions of the search space."
 abstract type AbstractNode end
 
-"""
-AbstractTracker implements an interface with methods to:
-- save data at a given node
-- get data at a given node
-- compute data to activate / deactivate in order to restore the searchspace 
-  to move from a node to another node.
-"""
-abstract type AbstractTracker end
-
 # Interface to implement
+"Returns the type of search space depending on the tree-seach algorithm and its parameters."
+search_space_type(::AbstractAlgorithm) = nothing
+
+"Creates and returns the search space of a tree search algorithm, its model, and its input."
+function new_space(::Type{SearchSpaceType}, alg, model, input) where SearchSpaceType <: AbstractSearchSpace
+  @warn "new_space(::Type{$SearchSpaceType}, ::$(typeof(alg)), ::$(typeof(model)), ::$(typeof(input))) not implemented."
+  return nothing
+end
+
 "Creates and returns the root node of a search space."
-new_root(::AbstractSearchSpace, ::AbstractTracker) = nothing
+new_root(::AbstractSearchSpace) = nothing
+
+# This method will have a default implementation in Coluna.
+"Evaluate and generate children."
+function children(sp, n, env)
+  @warn "children(::$(typeof(sp)), ::$(typeof(n)), ::$(typeof(env))) not implemented."
+  return nothing
+end
 
 "Creates and returns the children of a node associated to a search space."
-function new_children(candidates, a::AbstractAlgorithm, n::AbstractNode, sp::AbstractSearchSpace, t::AbstractTracker)
-  @warn "new_children($(typeof(candidates)), $(typeof(a)), $(typeof(n)), $(typeof(sp)), $(typeof(t))) not implemented."
+function new_children(sp::AbstractSearchSpace, candidates, n::AbstractNode)
+  @warn "new_children(::$(typeof(sp)), ::$(typeof(candidates)), ::$(typeof(n))) not implemented."
   return nothing
 end
 
@@ -44,47 +69,23 @@ priority(::AbstractExploreStrategy, ::AbstractNode) = nothing
 # "Returns the manager which is responsible for handling the kpis and the best know solution."
 # manager(::AbstractSearchSpace) = nothing
 
-# Methods specific to the tracker
-
-"A data structure that wraps a piece of information to track in the tree."
-abstract type AbstractTrackedData end
-
-function new_tracker(st::AbstractExploreStrategy, sp::AbstractSearchSpace)
-  @warn "new_tracker(::$(typeof(st)), ::$(typeof(sp))) not implemented."
-  return nothing
-end
-
-## TODO: we won't have an interface for the tracker because it is used only in methods
-## implemented by algorithms developers.
-
-# "Save a piece of information in the tracker for a given node."
-# save!(::AbstractTracker, ::AbstractNode, ::AbstractTrackedData) = nothing
-
-# "Returns a piece of information from the tracker for a given node."
-# Base.get(::AbstractTracker, ::AbstractNode, ::Type{AbstractTrackedData}) = nothing
-
+# TODO: not needed at the moment.
 # """
 # Computes data to activate / deactivate information in order to restore the searchspace 
 # to move from a node to another node.
 # """
-# diff(::AbstractTracker, src::AbstractNode, dest::AbstractNode, ::Type{AbstractTrackedData}) = nothing
+# diff(::AbstractTracker, src::AbstractNode, dest::AbstractNode) = nothing
 
 # Methods specific to the space (this is WIP)
-function get_reformulation(a::AbstractAlgorithm, s::AbstractSearchSpace)
-  @warn "get_reformulation(::$(typeof(a)), ::$(typeof(s))) not implemented."
+
+function get_input(a::AbstractAlgorithm, s::AbstractSearchSpace, n::AbstractNode)
+  @warn "get_input(::$(typeof(a)), ::$(typeof(s)), ::$(typeof(n))) not implemented."
   return nothing
 end
 
-function get_input(a::AbstractAlgorithm, s::AbstractSearchSpace, n::AbstractNode, t::AbstractTracker)
-  @warn "get_input(::$(typeof(a)), ::$(typeof(s)), ::$(typeof(n)), ::$(typeof(t))) not implemented."
+function node_change!(previous::AbstractNode, current::AbstractNode, space::AbstractSearchSpace)
+  @warn "node_change!(::$(typeof(previous)), $(typeof(current)), $(typeof(space))) not implemented."
   return nothing
 end
 
-function node_change!(previous::AbstractNode, current::AbstractNode, space::AbstractSearchSpace, tracker::AbstractTracker)
-  @warn "node_change!(::$(typeof(previous)), $(typeof(current)), $(typeof(space)), $(typeof(tracker))) not implemented."
-  return nothing
-end
-
-after_conquer!(space::AbstractSearchSpace, output) = nothing
-
-tree_search_output(space::AbstractSearchSpace) = nothing
+after_conquer!(::AbstractSearchSpace, output) = nothing
