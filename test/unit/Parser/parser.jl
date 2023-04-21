02ba09381b1917db050c70ea87547a8ebb5f249b
Guillaume Marques <guillaume.marques@protonmail.com>
2023-04-21 11:57:07 +0200

    rm focus

--- test/unit/Parser/parser.jl
+++ test/unit/Parser/parser.jl
@@ -6,7 +6,7 @@ function parser_strip_indentation()
     s = "   1.1*x_1"
     @test Parser._strip_identation(s) == "1.1*x_1"
 end
-register!(unit_tests, "parser", parser_strip_indentation; f = true)
+register!(unit_tests, "parser", parser_strip_indentation)
 
 function parser_strip_line()
     s = "1.1*x_1,2.2*y_1,3.3*z_1"
@@ -16,7 +16,7 @@ function parser_strip_line()
     s = "   1.1*x_1 , 2.2*y_1 , 3.3*z_1   "
     @test Parser._strip_line(s) == "1.1*x_1,2.2*y_1,3.3*z_1"
 end
-register!(unit_tests, "parser", parser_strip_line; f = true)
+register!(unit_tests, "parser", parser_strip_line)
 
 function parser_get_vars_list()
     s = "x_1,y_1,z_1"
@@ -26,7 +26,7 @@ function parser_get_vars_list()
     s = "   x_1 , y_1 , z_1   "
     @test Parser._get_vars_list(s) == ["x_1", "y_1", "z_1"]
 end
-register!(unit_tests, "parser", parser_get_vars_list; f = true)
+register!(unit_tests, "parser", parser_get_vars_list)
 
 function parser_read_expression()
     s = "x_1 + y_1 - z_1"
@@ -36,7 +36,7 @@ function parser_read_expression()
     s = "2*x_1 + 6*y_1 - 1.1z_1"
     @test Parser._read_expression(s).vars == Dict("y_1" => 6.0, "x_1" => 2.0, "z_1" => -1.1)
 end
-register!(unit_tests, "parser", parser_read_expression; f = true)
+register!(unit_tests, "parser", parser_read_expression)
 
 function parser_read_constraint()
     s = "=="
@@ -63,7 +63,7 @@ function parser_read_constraint()
     @test c.sense == ClMP.Greater
     @test c.rhs == 5.0
 end
-register!(unit_tests, "parser", parser_read_constraint; f = true)
+register!(unit_tests, "parser", parser_read_constraint)
 
 function parser_read_bounds()
     less_r = Regex("(($(Parser.coeff_re))<=)?([\\w,]+)(<=($(Parser.coeff_re)))?")
@@ -86,4 +86,4 @@ function parser_read_bounds()
     s = "21.5 >= x1, x2 >= 20"
     @test Parser._read_bounds(s, greater_r) == (["x1", "x2"], "21.5", "20")
 end
-register!(unit_tests, "parser", parser_read_bounds; f = true)
+register!(unit_tests, "parser", parser_read_bounds)
