6b5d1b8f38bbf4ed1b23752f594d49e36b580be5
Guillaume Marques <guillaume.marques@protonmail.com>
2023-04-21 11:57:37 +0200

    Update test/e2e/gap/generalized_assignment.jl

--- test/e2e/gap/generalized_assignment.jl
+++ test/e2e/gap/generalized_assignment.jl
@@ -23,7 +23,7 @@ function gap_toy_instance()
     @test MOI.get(model, MOI.NumberOfVariables()) == length(x)
     @test MOI.get(model, MOI.SolverName()) == "Coluna"
 end
-register!(e2e_tests, "gap", gap_toy_instance)
+register!(e2e_tests, "gap", gap_toy_instance; f = true)
 
 function gap_strong_branching()
     println("\e[45m gap strong branching \e[00m")
