a9d8c3a7216377820544fd09cc06a06315dc7e59
Guillaume Marques <guillaume.marques@protonmail.com>
2023-05-16 14:15:05 +0200

    rename advanced demo file

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ TUTORIAL_CUTS = joinpath(@__DIR__, "src", "start", "cuts.jl")
 TUTORIAL_PRICING = joinpath(@__DIR__, "src", "start", "pricing.jl")
 TUTORIAL_CUSTOMDATA = joinpath(@__DIR__, "src", "start", "custom_data.jl")
 TUTORIAL_INITCOLS = joinpath(@__DIR__, "src", "start", "initial_columns.jl")
-TUTORIAL_ADVANCED = joinpath(@__DIR__, "src", "start", "advanced-demo.jl")
+TUTORIAL_ADVANCED = joinpath(@__DIR__, "src", "start", "advanced_demo.jl")
 TUTORIAL_TREESEARCH_API = joinpath(@__DIR__, "src", "api", "treesearch.jl")
 TUTORIAL_STORAGE_API = joinpath(@__DIR__, "src", "api", "storage.jl")
 
