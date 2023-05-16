40c16b734affa4bd33be9aada87e0e6c2e7b6089
Guillaume Marques <guillaume.marques@protonmail.com>
2023-05-16 14:11:31 +0200

    fix typos in doc

--- docs/make.jl
+++ docs/make.jl
@@ -46,7 +46,7 @@ makedocs(
             "Pricing callback" => joinpath("start", "pricing.md"),
             "Custom data" => joinpath("start", "custom_data.md"),
             "Initial columns callback" => joinpath("start", "initial_columns.md"),
-            "Advanced demo" => joinpath("start", "advanced-demo.md"),
+            "Advanced tutorial" => joinpath("start", "advanced-demo.md"),
         ],
         "Manual" => Any[
             "Decomposition" => joinpath("man", "decomposition.md"),
