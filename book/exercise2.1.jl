57aef5a1c1bce1183d3c34a16a4cb7effa6a0e4d
Artur Alves Pessoa <artur.a.pessoa@gmail.com>
2023-07-27 19:47:50 -0300

    Adding two exercises and printing the result

--- book/exercise2.1.jl
+++ book/exercise2.1.jl
@@ -12,21 +12,23 @@ coluna = optimizer_with_attributes(
     Coluna.Optimizer,
     "params" => Coluna.Params(
         solver = Coluna.Algorithm.TreeSearchAlgorithm(), # default BCP
-        local_art_var_cost = 99.0
+        # local_art_var_cost = 99.0
     ),
     "default_optimizer" => GLPK.Optimizer # GLPK for the master & the subproblems
 );
 
 @axis(sp_ids, 1:1);
 model = BlockModel(coluna);
-@variable(model, x[1:3] >= 0)
-@objective(model, Min, 8x[1] + 13x[2] - 5x[3])
-@constraint(model, r1, 4x[1] + x[2] + 2x[3] == 5)
-@constraint(model, r2, x[1] + x[2] == 1)
-@constraint(model, r3[sp_ids[1]], -2x[1] + 3x[2] + 3x[3] <= 3)
-@constraint(model, r4[sp_ids[1]], 3x[1] - x[2] + 6x[3] <= 6)
-subproblems = column_generation(model, sp_ids, x[1], x[2], x[3])
+@variable(model, x[1:4] >= 0)
+@objective(model, Min, x[1] + 7x[2] + 2x[3] + 3x[4])
+@constraint(model, r1, 4x[1] + 2x[2] + 2x[3] + 3x[4] <= 30)
+@constraint(model, r2, x[1] + x[2] + x[3] + 2x[4] == 15)
+@constraint(model, r3[sp_ids[1]], 2x[1] + 3x[2] - 7x[3] - 6x[4] == 0)
+@constraint(model, r4[sp_ids[1]], -x[2] + 2x[3] <= 0)
+@constraint(model, r5[sp_ids[1]],  x[2] - 2x[3] - 2x[4] <= 0)
+@constraint(model, r6[sp_ids[1]], -x[2] + 3x[3] + 2x[4] <= 2)
+subproblems = column_generation(model, sp_ids, x[1], x[2], x[3], x[4])
 specify!(subproblems[1], lower_multiplicity = 0, upper_multiplicity = 1)
 optimize!(model);
-x̄ = [value(x[1]); value(x[2]); value(x[3])]
+x̄ = [value(x[1]); value(x[2]); value(x[3]); value(x[4])]
 @show x̄
--- book/exercise2.4.jl
+++ book/exercise2.4.jl
@@ -21,16 +21,17 @@ coluna = optimizer_with_attributes(
 
 @axis(sp_ids, 1:2);
 model = BlockModel(coluna);
-@variable(model, x[1:4] >= 0) # TODO (page 36)
-@objective(model, Min, 3x[1] + 7x[2] + 2x[3] - x[4])
-@constraint(model, r1, 2x[2] + x[3] - x[4] >= 7)
-@constraint(model, r2, x[1] + x[2] + 2x[3] + 3x[4] == 12)
-@constraint(model, r3[sp_ids[1]], x[1] + x[2] <= 4)
-@constraint(model, r4[sp_ids[1]], 3x[1] + x[2] <= 6)
-@constraint(model, r5[sp_ids[2]], x[3] + x[4] <= 5)
-subproblems = column_generation(model, sp_ids, (x[1], x[2]), (x[3], x[4]))
-specify!(subproblems[1], lower_multiplicity = 0, upper_multiplicity = 2)
+@variable(model, x[1:5] >= 0)
+@objective(model, Min, -x[1] + 3x[2] - 6x[3] - 7x[4] + 9x[5])
+@constraint(model, r1,  x[1] + 2x[2]         -  x[4] + 3x[5] == 36)
+@constraint(model, r2[sp_ids[1]], -x[2] + 2x[3] <= 6)
+@constraint(model, r3[sp_ids[1]], x[1] + x[2] + x[3] <= 10)
+@constraint(model, r4[sp_ids[2]], 2x[4] + x[5] <= 22)
+@constraint(model, r5[sp_ids[2]], x[5] <= 10)
+@constraint(model, r6[sp_ids[2]], 3x[4] + x[5] <= 30)
+subproblems = column_generation(model, sp_ids, (x[1], x[2], x[3]), (x[4], x[5]))
+specify!(subproblems[1], lower_multiplicity = 0, upper_multiplicity = 1)
 specify!(subproblems[2], lower_multiplicity = 0, upper_multiplicity = 1)
 optimize!(model);
-x̄ = [value(x[1]); value(x[2]); value(x[3]); value(x[4])]
+x̄ = [value(x[1]); value(x[2]); value(x[3]); value(x[4]); value(x[5])]
 @show x̄
