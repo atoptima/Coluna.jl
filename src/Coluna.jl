7b64fed9277287e06a3500bd265dac17e07164d7
Guillaume Marques <guillaume.marques@protonmail.com>
2023-09-05 10:32:00 +0200

    v0.6.6 (#1049)

--- src/Coluna.jl
+++ src/Coluna.jl
@@ -29,7 +29,7 @@ export Algorithm, ColunaBase, MathProg, Env, DefaultOptimizer, Parameters,
 
 const _to = TO.TimerOutput()
 
-version() = v"0.6.5"
+version() = v"0.6.6"
 
 include("kpis.jl")
 include("parameters.jl")
