aea8ba1b1fe34cf0112a3740d114f544aa759b6d
Guillaume Marques <guillaume.marques@protonmail.com>
2023-09-07 15:41:16 +0200

    limited discrepancy search explore strategy

--- src/TreeSearch/explore.jl
+++ src/TreeSearch/explore.jl
@@ -1,19 +1,16 @@
+"Generic implementation of the tree search algorithm for a given explore strategy."
+@mustimplement "TreeSearch" tree_search(s::AbstractExploreStrategy, space, env, input) = nothing
+
+
+################################################################################
+# Depth First Strategy
+################################################################################
+
 """
 Explore the tree search space with a depth-first strategy.
 The next visited node is the last one pushed in the stack of unexplored nodes.
 """
 struct DepthFirstStrategy <: AbstractExploreStrategy end
-
-abstract type AbstractBestFirstSearch <: AbstractExploreStrategy end
-
-"""
-Explore the tree search space with a best-first strategy.
-The next visited node is the one with the highest local dual bound.
-"""
-struct BestDualBoundStrategy <: AbstractBestFirstSearch end
-
-"Generic implementation of the tree search algorithm for a given explore strategy."
-@mustimplement "TreeSearch" tree_search(s::AbstractExploreStrategy, space, env, input) = nothing
 
 function tree_search(::DepthFirstStrategy, space, env, input)
     root_node = new_root(space, input)
@@ -28,6 +25,18 @@ function tree_search(::DepthFirstStrategy, space, env, input)
     return TreeSearch.tree_search_output(space, stack)
 end
 
+################################################################################
+# Best First Strategy
+################################################################################
+
+abstract type AbstractBestFirstSearch <: AbstractExploreStrategy end
+
+"""
+Explore the tree search space with a best-first strategy.
+The next visited node is the one with the highest local dual bound.
+"""
+struct BestDualBoundStrategy <: AbstractBestFirstSearch end
+
 function tree_search(strategy::AbstractBestFirstSearch, space, env, input)
     root_node = new_root(space, input)
     pq = PriorityQueue{typeof(root_node), Float64}()
@@ -39,4 +48,43 @@ function tree_search(strategy::AbstractBestFirstSearch, space, env, input)
         end
     end
     return TreeSearch.tree_search_output(space, pq)
+end
+
+################################################################################
+# Limited Discrepancy
+################################################################################
+
+struct LimitedDiscrepancyStrategy <: AbstractExploreStrategy
+    max_discrepancy::Int
+end
+
+struct LimitedDiscrepancySpace <: AbstractSearchSpace
+    inner_space::AbstractSearchSpace
+    max_discrepancy::Int
+end
+struct LimitedDiscrepancyNode
+    inner_node::AbstractNode
+    discrepancy::Int
+end
+
+new_root(space::LimitedDiscrepancySpace, input) = LimitedDiscrepancyNode(new_root(space.inner_space, input), space.max_discrepancy)
+stop(space::LimitedDiscrepancySpace, nodes) = stop(space.inner_space, nodes)
+tree_search_output(space::LimitedDiscrepancySpace, nodes) = tree_search_output(space.inner_space, nodes)
+
+function children(space::LimitedDiscrepancySpace, current::LimitedDiscrepancyNode, env, input)
+    lds_children = LimitedDiscrepancyNode[]
+    inner_children = children(space.inner_space, current.inner_node, env, input)
+    for (i, child) in enumerate(inner_children)
+        discrepancy = current.discrepancy - i + 1
+        if discrepancy < 0
+            break
+        end
+        pushfirst!(lds_children, LimitedDiscrepancyNode(child, discrepancy))
+    end
+    return lds_children
+end
+
+function tree_search(strategy::LimitedDiscrepancyStrategy, space, env, input)
+    space = LimitedDiscrepancySpace(space, strategy.max_discrepancy)
+    return tree_search(DepthFirstStrategy(), space, env, input)
 end
